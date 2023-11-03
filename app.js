e82ca8a3f05ebda2ffcffc09baed5116742d489b
Ayan-farhad <khaanayan81@gmail.com>
2023-11-03 02:58:30 -0700

    continue

---
+++ app.js
@@ -0,0 +1,32 @@
+        //**Class assginmemt **/
+
+// function anyTable() {
+    
+// var number = parseInt(document.getElementById('numberInput').value);
+
+// if (!isNaN(number)) {
+
+// var table = "<center>";
+
+// for (var i = 1; i <= 10; i++) {
+
+// var result = number * i;
+
+// table +=  number + " x " + i +" = "+ result +"</br>";
+// }
+// table += "</table>";
+
+//  document.getElementById('tableContainer').innerHTML = table;
+
+// } else {
+//  document.getElementById('tableContainer').innerHTML = "Invalid input. Please enter a valid number.";
+// }
+// }
+        
+
+
+// xx-----------------------------------xx
+
+
+
+
