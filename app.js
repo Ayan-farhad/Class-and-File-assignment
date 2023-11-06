b4bb6999a330391c65991baddc232584952d34bf
Ayan-farhad <khaanayan81@gmail.com>
2023-11-06 04:02:26 -0800

    array

--- app.js
+++ app.js
@@ -92,11 +92,10 @@
 
 // var userValue = prompt("Enter a number:");
 
-// var number = parseInt(userInput);
 
-// if (number % 3 && number % 3) {
+// if (userValue < 3 && userValue <= 3) {
    
-//         console.log("Number is divisible by 3");
+//      console.log("Number is divisible by 3");
        
 // }else{
 //      console.log("Number is not divisible by 3");
@@ -105,16 +104,19 @@
 
 // Question 6
 
-// var userInput = prompt("Enter a character:");       R
+// var userInput = prompt("Enter a character:");      
 
 // var charCode = userInput.charCodeAt(0);
 
 // if (charCode >= 48 && charCode <= 57) {
 //     console.log("The input is a number.");
+
 // } else if (charCode >= 65 && charCode <= 90) {
 //     console.log("The input is an uppercase letter.");
+
 // } else if (charCode >= 97 && charCode <= 122) {
 //     console.log("The input is a lowercase letter.");
+
 // } else {
 //     console.log("Invalid input.");
 // }
@@ -289,9 +291,95 @@
 //         document.write("Sorry, you have to wait, or go get a snack.")
 // }
 
-// Question 16
+       //ASSIGNMENT ARRAY 2 
 
-// pending
+//question 1
+
+// let studentNames = [];
+
+// studentNames.push("Taha");
+// studentNames.push("Saad");
+// studentNames.push("Abdullah");
+
+// console.log(studentNames); 
+
+//question 2
+
+// let studentNames = new Array();
+// let studentNames = [];
 
 
-// Question 17
+//question 3
+
+// let colors = ["red", "green", "blue", "yellow"];
+
+
+//question 4
+
+// let numbers = [1, 2, 3, 4, 5];
+
+
+//question 5
+
+// let flags = [true, false, true, false, true];
+
+
+//question 6
+
+
+
+//question 7
+
+// let networks = ["Mobilink", "Telenor", "Zong", "Ufone", "Warid"];
+
+// console.log("Available mobile networks in Pakistan:");
+// for (let i = 0; i < networks.length; i++) {
+//     console.log(networks[i]);
+// }
+
+
+//question 8
+
+// let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// document.write("<h2>Available Education Qualifications in Pakistan:</h2>");
+// document.write("<ol>");
+
+// for (let i = 0; i < qualifications.length; i++) {
+//     document.write("<li>" + qualifications[i] + "</li>");
+// }
+
+// document.write("</ol>");
+
+
+//question 9
+
+// let topMovies2015 = [];
+
+// topMovies2015.push("Avengers: Age of Ultron");
+// topMovies2015.push("Star Wars: The Force Awakens");
+// topMovies2015.push("Jurassic World");
+// topMovies2015.push("Inside Out");
+// topMovies2015.push("Mad Max: Fury Road");
+
+// document.write("<h2>Top Movies of 2015:</h2>");
+// document.write("<ul>");
+// for (let i = 0; i < topMovies2015.length; i++) {
+//     document.write("<li>" + topMovies2015[i] + "</li>");
+// }
+// document.write("</ul>");
+
+// document.write("<p>Number of movies in the list: " + topMovies2015.length + "</p>");
+
+
+//question 10
+
+// let favoriteCars = ["Tesla Model S", "Porsche 911", "Audi R8", "BMW M3", "Mercedes-Benz S-Class"];
+
+// console.log("First index of the array: " + favoriteCars[0]);
+
+// console.log("Car at first index of the array: " + favoriteCars[0]);
+
+// console.log("Last index of the array: " + (favoriteCars.length - 1));
+
+// console.log("Car at last index of the array: " + favoriteCars[favoriteCars.length - 1]);
