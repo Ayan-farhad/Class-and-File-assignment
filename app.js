1b1c45a21fca6c70cdb9e671bedb93c58958708e
Ayan-farhad <khaanayan81@gmail.com>
2023-11-06 05:49:18 -0800

    loops

--- app.js
+++ app.js
@@ -383,3 +383,306 @@
 // console.log("Last index of the array: " + (favoriteCars.length - 1));
 
 // console.log("Car at last index of the array: " + favoriteCars[favoriteCars.length - 1]);
+
+// LOOPS //
+
+// question 1
+
+// for (let i = 0; i < 5; i++){
+//     document.write('Hello to the World of Loops <br>')
+// }
+
+//question 2
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+//question 3
+
+// const number = +prompt('Enter any number to write its multiplication table:')
+// const length = +prompt('Enter the lenght of multiplication table:')
+
+// if (!isNaN(number) && !isNaN(length)){
+//     document.write(`<h2>Multiplication Table of ${[number]} </h2>`)
+//     document.write('<ul>')
+//     for (let i = 1; i <= length; i++){
+//         const result = number * i;
+//         document.write(`<li>${[number]} x ${[i]} = ${[result]} </li>`)
+//     }
+//     document.write('</ul>')
+// } else {
+//     document.write(`<h2>Please enter a valid input number</h2>`)
+// }
+
+//question 4
+
+// let A = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"];
+// for (let i = 0; i < A.length; i++){
+//     console.log(A[i])
+// }
+
+
+//question 5
+
+// let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+// for (let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+//question 6 AI
+
+// Ask the user for the number of items
+// var N = parseInt(prompt("Enter the number of items for the array:"));
+
+// if (!isNaN(N) && N > 0) {
+//     var itemsArray = [];
+
+//     for (var i = 0; i < N; i++) {
+//         var item = prompt("Enter item " + (i + 1) + ":");
+//         itemsArray.push(item);
+//     }
+
+//     console.log("Array of items: " + itemsArray);
+// } else {
+//     console.log("Invalid input. Please enter a valid number greater than 0.");
+// }
+
+
+//question 7
+
+// document.write(`<h2>Counting: </h2>`)
+// for (i = 1; i <= 15; i++){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Reverse Counting: </h2>')
+// for (let i = 10; i >= 1; i--){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Even: </h2>')
+// for (let i = 0; i <= 20; i += 2){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Odd: </h2>')
+// for (let i = 1; i <= 19; i += 2){
+//     document.write(`${[i]}, `)
+// }
+
+// document.write('<h2>Series: </h2>')
+// for (let i = 2; i <= 20; i += 2){
+//     document.write(`${[i]}k, `)
+// }
+
+
+//question 8 AI
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var userInput = prompt("Enter the item you want to search:");
+
+// var userInputLowercase = userInput.toLowerCase();
+
+// var isFound = false;
+
+// for (var i = 0; i < A.length; i++) {on
+    // var itemLowercase = A[i].toLowerCase();
+    // if (userInputLowercase === itemLowercase) {
+        // isFound = true;
+        // break;
+    // }
+// }
+
+
+// if (isFound) {
+//     alert("Yes, '" + userInput + "' is found in the list.");
+// } else {
+//     alert("No, '" + userInput + "' is not found in the list.");
+// }
+
+
+//question 9
+
+// let A = [24, 53, 78, 91, 12]
+// let largestNum = A[0]
+
+// for (let i = 1; i < A.length; i++){
+//     if (A[i] > largestNum){
+//         largestNum = A[i]
+//     }
+// }
+// console.log(`The Largest number in the Array is ${[largestNum]}`);
+
+
+//question 10
+
+// let A = [24, 53, 78, 91, 12]
+// let smallestNum = A[0]
+
+// for (let i = 1; i < A.length; i++){
+//     if (A[i] < smallestNum){
+//         smallestNum = A[i]
+//     }
+// }
+// console.log(`The smallest number in the Array is ${[smallestNum]}`);
+
+
+//question 11
+
+// let A = [24, 53, 78, 91, 12]
+
+// let largeNum = A[0]
+// let SmallNum = A[0]
+
+// for (let i = 1; i < A.length; i++){
+//     if (A[i] > largeNum){
+//         largeNum = A[i]
+//     } else if (A[i] < SmallNum){
+//         SmallNum = A[i]
+//     }
+// }
+// console.log(`The Array is ${[A]}`)
+// console.log("The largest number in the array is: " + largeNum)
+// console.log("The smallest number in the array is: " + SmallNum)
+
+//question 12
+
+// for (let i = 5; i <= 100; i += 5){
+//     document.write(`${[i]}, `)
+// }
+
+//question 13
+
+// let students = ["Ali", "Sami", "Taha", "Inam"]
+// let scores = [58, 73, 89, 90]
+
+// let table = "<table border='1'><tr><th>Student Name</th><th>Score</th></tr>"
+
+// for ( i = 0; i < students.length; i++){
+//     table += `<tr><td> ${[students[i]]} </td><td> ${[scores[i]]}</td> </tr>`
+// }
+// table += '</table>'
+// document.write(table)
+
+
+//question 14 
+
+// var scores = [12, 45, 3, 22, 34, 50];
+
+// var stopValue = parseInt(prompt("Enter the stop value:"));
+
+// if (!isNaN(stopValue)) {
+//     var output = [];
+//     for (var i = 0; i < scores.length; i++) {
+//         if (scores[i] <= stopValue) {
+//             output.push(scores[i]);
+//         } else {
+//             break;
+//         }
+//     }
+    
+//     console.log(output);
+// } else {
+//     console.log("Invalid input. Please enter a valid number.");
+// }
+
+
+//question 15
+
+// Given 2D array
+// var A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+// for (var i = 0; i < A.length; i++) {
+//     for (var j = 0; j < A[i].length; j++) {
+//         console.log(A[i][j]);
+//     }
+// }
+
+
+//question 16
+
+// var num = parseFloat(prompt("Enter a number:"));
+
+// if (!isNaN(num)) {
+//     while (num > 0) {
+//         console.log(num);
+//         num -= 0.5;
+//     }
+// } else {
+//     console.log("Invalid input. Please enter a valid number.");
+// }
+
+
+//question 17
+
+// for (var i = 0; i <= 20; i++) {
+//     // Checking if the current number is even or odd
+//     if (i % 2 === 0) {
+//         console.log(i + " is even");
+//     } else {
+//         console.log(i + " is odd");
+//     }
+// }
+
+
+//question 18
+
+// var product = 1;
+
+// for (var i = 1; i <= 7; i++) {
+//     if (i % 2 !== 0) {
+//         product *= i;
+//     }
+// }
+
+// console.log("Product of odd integers from 1 to 7: " + product);
+
+
+//question 19
+
+// var initialStars = parseInt(prompt("Enter the initial number of stars:"));
+
+// if (!isNaN(initialStars) && initialStars > 0) {
+//     // Loop to print lines of stars with decreasing count
+//     for (var i = initialStars; i > 0; i--) {
+//         var line = '';
+//         for (var j = 0; j < i; j++) {
+//             line += '*';
+//         }
+//         console.log(line);
+//     }
+// } else {
+//     console.log("Invalid input. Please enter a valid positive number.");
+// }
+
+
+//question 20
+
+
+// for (var i = 0; i < numLines; i++) {
+//     var line = '';
+//     for (var j = 0; j < 5; j++) {
+//         line += '*';
+//     }
+//     document.write(line + '<br>');
+// }
+// document.write('<br>');
+
+// for (var i = 1; i <= numLines; i++) {
+//     var line = '';
+//     for (var j = 0; j < i; j++) {
+//         line += '*';
+//     }
+//     document.write(line + '<br>');
+// }
+// document.write('<br>');
+
+// for (var i = numLines; i > 0; i--) {
+//     var line = '';
+//     for (var j = 0; j < i; j++) {
+//         line += '*';
+//     }
+//     document.write(line + '<br>');
+// }
